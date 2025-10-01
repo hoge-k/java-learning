75294ecfe70649981f63922744f6f695d2b29ad9
boku-ha-saboten <kakemasato1211@gmail.com>
2025-10-01 23:11:16 +0900

    第5章

---
+++ Overload/Main.java
@@ -0,0 +1,17 @@
+class Test {
+    void myPrint() { System.out.println("myPrint()"); }
+    void myPrint(String s) { System.out.println("myPrint(String s)");}
+    void myPrint(int a) { System.out.println("myPrint(int a)");}
+    void myPrint(int a, int b) {
+        System.out.println("myPrint(int a, int b)"); }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Test t = new Test(); // Testクラスのインスタンス化
+        t.myPrint();
+        t.myPrint(100);
+        t.myPrint(100, 200);
+        t.myPrint("yamadahouse");
+    }
+}
---
+++ Overload/Overload2/Main.java
@@ -0,0 +1,17 @@
+class Employee {
+    int id; String name;
+    Employee() { System.out.println("Employee()"); }
+    Employee(String name) { System.out.println("Employee(String name)"); }
+    Employee(int id) { System.out.println("Employee(int id)"); }
+    Employee(int id, String name) {
+        System.out.println("Employee(int id, String name)"); }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Employee a = new Employee();
+        Employee b = new Employee("yamada house");
+        Employee c = new Employee(100);
+        Employee d = new Employee(100, "yamada house");
+    }
+}
